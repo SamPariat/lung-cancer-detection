5a2d3c5f9c3194bede9550e59dde005cae69b2c0
Sam <apunthelegend@outlook.com>
2024-05-03 13:42:06 +0530

    Added json tags

--- cli/cmd/predict-result/model.go
+++ cli/cmd/predict-result/model.go
@@ -1,20 +1,20 @@
 package predict_result
 
 type CLIResult struct {
-	Gender               string
-	Age                  int
-	Smoking              int
-	YellowFingers        int
-	Anxiety              int
-	PeerPressure         int
-	ChronicDisease       int
-	Fatigue              int
-	Allergy              int
-	Wheezing             int
-	AlcoholConsuming     int
-	Coughing             int
-	ShortnessOfBreath    int
-	SwallowingDifficulty int
-	ChestPain            int
-	LungCancer           string
+	Gender               string `json:"gender"`
+	Age                  int    `json:"age"`
+	Smoking              int    `json:"smoking"`
+	YellowFingers        int    `json:"yellowFingers"`
+	Anxiety              int    `json:"anxiety"`
+	PeerPressure         int    `json:"peerPressure"`
+	ChronicDisease       int    `json:"chronicDisease"`
+	Fatigue              int    `json:"fatigue"`
+	Allergy              int    `json:"allergy"`
+	Wheezing             int    `json:"wheezing"`
+	AlcoholConsuming     int    `json:"alcoholConsuming"`
+	Coughing             int    `json:"coughing"`
+	ShortnessOfBreath    int    `json:"shortnessOfBreath"`
+	SwallowingDifficulty int    `json:"swallowingDifficulty"`
+	ChestPain            int    `json:"chestPain"`
+	LungCancer           string `json:"lungCancer"`
 }
